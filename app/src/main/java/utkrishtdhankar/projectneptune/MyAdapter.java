2ed5db2c07a066343f5cbbbec630906b5e396109
Utkrisht Dhankar <utkrishtdhankar@gmail.com>
2016-11-07 16:36:19 +0530

    The inbox now shows status of the task

--- app/src/main/java/utkrishtdhankar/projectneptune/MyAdapter.java
+++ app/src/main/java/utkrishtdhankar/projectneptune/MyAdapter.java
@@ -21,10 +21,12 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
     public static class ViewHolder extends RecyclerView.ViewHolder {
         // each data item is just a string in this case
         public TextView mTextView;
+        public TextView statusView;
         public ViewHolder(View v) {
             super(v);
             //Linking the TextView present in R.layout.card_layout
             mTextView = (TextView) v.findViewById(R.id.info_text);
+            statusView = (TextView) v.findViewById(R.id.status_text);
         }
     }
     //TODO work with array lists instead of string sets
@@ -52,7 +54,7 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
         // - get element from your dataset at this position
         // - replace the contents of the view (TextView) with that element's info
         holder.mTextView.setText(mDataset.get(position).getName());
-
+        holder.statusView.setText(mDataset.get(position).getStatus().name());
     }
 
     // Return the size of your dataset (invoked by the layout manager)
