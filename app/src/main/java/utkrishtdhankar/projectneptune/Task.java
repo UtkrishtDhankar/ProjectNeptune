0a42de7b8943b680eec9deb6e58007d625b2e87e
Utkrisht Dhankar <utkrishtdhankar@gmail.com>
2016-11-07 18:25:18 +0530

    Refactored tags as contexts

--- app/src/main/java/utkrishtdhankar/projectneptune/Task.java
+++ app/src/main/java/utkrishtdhankar/projectneptune/Task.java
@@ -10,8 +10,8 @@ public class Task {
     // The name of this task, e.g "Do the laundry"
     private String name;
 
-    // The tags list for this task
-    private ArrayList<String> tags;
+    // The contexts list for this task
+    private ArrayList<String> contexts;
 
     // What is the status of the task, e.g. Next, Waiting etc.
     private TaskStatus status;
@@ -23,7 +23,7 @@ public class Task {
      */
     Task(String taskName) {
         name = taskName;
-        tags = new ArrayList<String>();
+        contexts = new ArrayList<String>();
         status = TaskStatus.Inbox;
     }
 
@@ -42,32 +42,33 @@ public class Task {
     void changeStatus(TaskStatus newStatus) {
         status = newStatus;
     }
+    
     /**
-     * Adds a new tag to the task
-     * @param newTag The tag to be added
+     * Adds a new context to the task
+     * @param newContext The context to be added
      */
-    public void addTag(String newTag) {
-        if (!newTag.isEmpty() && !tags.contains(newTag)) {
-            tags.add(newTag);
+    public void addContext(String newContext) {
+        if (!newContext.isEmpty() && !contexts.contains(newContext)) {
+            contexts.add(newContext);
         }
     }
 
     /**
      * Removes a tag from the task.
      * If the given tag exists, it will be removed.
-     * @param tagToBeRemoved The tag that will be searched for and removed
+     * @param contextToBeRemoved The tag that will be searched for and removed
      */
-    public void removeTag(String tagToBeRemoved) {
-        // remove all tags which are the same as tagToBeRemoved
-        tags.removeAll(Collections.singleton(tagToBeRemoved));
+    public void removeTag(String contextToBeRemoved) {
+        // remove all contexts which are the same as tagToBeRemoved
+        contexts.removeAll(Collections.singleton(contextToBeRemoved));
     }
 
     /**
-     * Returns all the tags accociated with this task
-     * @return an array of all the tags
+     * Returns all the contexts associated with this task
+     * @return an array of all the contexts
      */
-    public ArrayList<String> getAllTags() {
-        return tags;
+    public ArrayList<String> getAllContexts() {
+        return contexts;
     }
 
     /**
