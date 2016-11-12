163af9286bb9674ffd668957682c38f9becf6489
Utkrisht Dhankar <utkrishtdhankar@gmail.com>
2016-11-12 10:34:34 +0530

    Added comments to DatabaseHelper
    This is not final as a database overhaul is already in the works

--- app/src/main/java/utkrishtdhankar/projectneptune/DatabaseHelper.java
+++ app/src/main/java/utkrishtdhankar/projectneptune/DatabaseHelper.java
@@ -11,25 +11,44 @@ import java.util.ArrayList;
 
 /**
  * Created by utkrishtdhankar on 21/10/16.
+ *
+ * This contains all the database related code. This can read, write, update etc. all values from
+ * the database, and is to be used for all such operations.
  */
 public class DatabaseHelper extends SQLiteOpenHelper{
+    // The version of the database that ships with this version of the app
+    // Update this if you make changes to the database so that it autoupdates the app
     private static final int DATABASE_VERSION = 1;
+
+    // The name of the database
     private static final String DATABASE_NAME = "projectNeptune";
 
+    // Parameters related to the inbox table. The name and all of it's column names go here.
     private static final String INBOX_TABLE_NAME = "inbox";
     private static final String INBOX_KEY_ID = "id";
     private static final String INBOX_KEY_NAME = "name";
     private static final String INBOX_KEY_STATUS = "status";
 
+    // Parameters related to the contexts table. The name and all of it's column names go here.
     private static final String CONTEXTS_TABLE_NAME = "contexts";
     private static final String CONTEXTS_KEY_ID = "id";
     private static final String CONTEXTS_KEY_NAME = "name";
     private static final String CONTEXTS_KEY_TASK_ID = "taskId";
 
+    /**
+     * Creates a new DatabaseHelper
+     * @param context the context of the activity
+     */
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
+    /**
+     * Called when the database needs to create all the tables.
+     * Will automatically be called when the database version is updated,
+     * or when no database exists.
+     * @param db
+     */
     @Override
     public void onCreate(SQLiteDatabase db) {
         final String createInboxQuery = String.format(
@@ -50,6 +69,13 @@ public class DatabaseHelper extends SQLiteOpenHelper{
         db.execSQL(createContextsQuery);
     }
 
+    /**
+     * Called when database version changes.
+     * Calls onCreate.
+     * @param db the database we're working with
+     * @param oldVersion the old version of the database
+     * @param newVersion the new version of the database
+     */
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS " + INBOX_TABLE_NAME);
@@ -57,15 +83,23 @@ public class DatabaseHelper extends SQLiteOpenHelper{
         onCreate(db);
     }
 
+    /**
+     * Adds the task given to the database
+     * Does _not_ check if such a task already exists, so this might add duplicates
+     * @param task the task to add
+     */
     void addTask(Task task) {
         SQLiteDatabase db = this.getWritableDatabase();
 
+        // Put in the values for this task into a contentvalues
         ContentValues inboxValues = new ContentValues();
         inboxValues.put(INBOX_KEY_NAME, task.getName());
         inboxValues.put(INBOX_KEY_STATUS, task.getStatus().name());
 
+        // Insert the task into the database and get it's id
         long newTaskId = db.insert(INBOX_TABLE_NAME, null, inboxValues);
 
+        // Add all of the task's contexts into the database as well
         ArrayList<TaskContext> contexts = task.getAllContexts();
         for (TaskContext context : contexts) {
             ContentValues contextsValues = new ContentValues();
@@ -78,6 +112,9 @@ public class DatabaseHelper extends SQLiteOpenHelper{
         db.close();
     }
 
+    /**
+     * @return all the tasks in the database
+     */
     ArrayList<Task> getAllTasks() {
         SQLiteDatabase db = this.getReadableDatabase();
 
@@ -131,6 +168,11 @@ public class DatabaseHelper extends SQLiteOpenHelper{
         return tasks;
     }
 
+    /**
+     * Finds a task by it's id and returns it
+     * @param id the thing to search for
+     * @return the task
+     */
     Task getTaskByID(long id) {
         SQLiteDatabase db = this.getReadableDatabase();
 
