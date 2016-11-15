94070df5bec7063fcfdcc091ee991d96c40afae5
Utkrisht Dhankar <utkrishtdhankar@gmail.com>
2016-11-15 18:48:45 +0530

    Added the Inbox class

--- app/src/main/java/utkrishtdhankar/projectneptune/TaskStatus.java
+++ app/src/main/java/utkrishtdhankar/projectneptune/TaskStatus.java
@@ -7,13 +7,13 @@ package utkrishtdhankar.projectneptune;
 // All statuses extend this
 public interface TaskStatus {
     // Encodes the task status for the database
-    String toString();
+    public String encode();
 
     // Gets the name of the status, e.g Inbox, Done, Scheduled, etc.
     // Use for UI.
-    String getName();
+    public String getName();
 
     // This gets the thing that should be appended to the status in the UI.
     // E.g, this will return the date a task is Scheduled, or the person we are waiting for.
-    String getSpecial();
+    public String getSpecial();
 }
