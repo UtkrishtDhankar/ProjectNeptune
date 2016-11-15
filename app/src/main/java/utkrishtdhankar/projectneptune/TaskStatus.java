14daf60d115ab86aa0afff2616f55d181288faa0
Utkrisht Dhankar <utkrishtdhankar@gmail.com>
2016-11-15 18:39:56 +0530

    Added a TaskStatus interface

--- app/src/main/java/utkrishtdhankar/projectneptune/TaskStatus.java
+++ app/src/main/java/utkrishtdhankar/projectneptune/TaskStatus.java
@@ -4,10 +4,16 @@ package utkrishtdhankar.projectneptune;
  * Created by utkrishtdhankar on 07/11/16.
  */
 
-public enum TaskStatus {
-    Inbox,
-    Next,
-    Waiting,
-    Scheduled,
-    Someday
+// All statuses extend this
+public interface TaskStatus {
+    // Encodes the task status for the database
+    String toString();
+
+    // Gets the name of the status, e.g Inbox, Done, Scheduled, etc.
+    // Use for UI.
+    String getName();
+
+    // This gets the thing that should be appended to the status in the UI.
+    // E.g, this will return the date a task is Scheduled, or the person we are waiting for.
+    String getSpecial();
 }
