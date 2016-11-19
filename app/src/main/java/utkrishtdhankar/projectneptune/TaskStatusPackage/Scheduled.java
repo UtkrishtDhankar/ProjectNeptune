998ec793f1a84a61aa9115ded077b72bdcbbe915
Utkrisht Dhankar <utkrishtdhankar@gmail.com>
2016-11-19 10:56:04 +0530

    Javadoc'd Scheduled

--- app/src/main/java/utkrishtdhankar/projectneptune/TaskStatusPackage/Scheduled.java
+++ app/src/main/java/utkrishtdhankar/projectneptune/TaskStatusPackage/Scheduled.java
@@ -7,20 +7,28 @@ import java.util.Calendar;
  * Created by utkrishtdhankar on 15/11/16.
  */
 
+/**
+ * Represents a task scheduled for execution on a later, specific date.
+ */
 public class Scheduled implements TaskStatus {
     private static final String name = "Scheduled";
 
     private Calendar scheduledForDate;
 
+    /**
+     * Sets the date for this task as the supplied parameter. There must never be a Scheduled
+     * without a date, so there's no default constructor.
+     * @param date the date to set this Scheduled to
+     */
     public Scheduled(Calendar date) {
         scheduledForDate = date;
     }
 
     /**
      * This returns a status. If scheduled is scheduled for a later date, then it returns it back.
-     * Otherwise
-     * @param scheduled
-     * @return
+     * Otherwise returns a Next object
+     * @param scheduled the status
+     * @return the new value of this status
      */
     public static TaskStatus updateScheduledStatus(Scheduled scheduled) {
         // If the time has passed, then return a next status, otherwise return scheduled
@@ -31,10 +39,17 @@ public class Scheduled implements TaskStatus {
         }
     }
 
+    /**
+     * @return the date that this is scheduled for, as a Calendar object
+     */
     public Calendar getScheduledForDate() {
         return scheduledForDate;
     }
 
+    /**
+     * Sets the date when this task is supposed to be executed
+     * @param scheduledForDate the new date
+     */
     public void setScheduledForDate(Calendar scheduledForDate) {
         this.scheduledForDate = scheduledForDate;
     }
