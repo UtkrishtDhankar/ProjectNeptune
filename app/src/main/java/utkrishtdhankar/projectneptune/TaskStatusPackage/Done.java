7c379525b262106ba81dec9610946112ee9f32e8
Utkrisht Dhankar <utkrishtdhankar@gmail.com>
2016-11-19 10:57:51 +0530

    Javadoc'd Done

--- app/src/main/java/utkrishtdhankar/projectneptune/TaskStatusPackage/Done.java
+++ app/src/main/java/utkrishtdhankar/projectneptune/TaskStatusPackage/Done.java
@@ -4,6 +4,9 @@ package utkrishtdhankar.projectneptune.TaskStatusPackage;
  * Created by utkrishtdhankar on 16/11/16.
  */
 
+/**
+ * This represents a task that has been completed
+ */
 public class Done implements TaskStatus {
 
     private static final String name = "Done";
