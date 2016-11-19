5472d0ad4eac295482afe5f043c797b8345cafb1
Utkrisht Dhankar <utkrishtdhankar@gmail.com>
2016-11-19 10:53:56 +0530

    Added a class description of Someday

--- app/src/main/java/utkrishtdhankar/projectneptune/TaskStatusPackage/Someday.java
+++ app/src/main/java/utkrishtdhankar/projectneptune/TaskStatusPackage/Someday.java
@@ -4,6 +4,9 @@ package utkrishtdhankar.projectneptune.TaskStatusPackage;
  * Created by utkrishtdhankar on 15/11/16.
  */
 
+/**
+ * This represents a task status that is to be done someday in the future
+ */
 public class Someday implements TaskStatus {
     private static final String name = "Someday";
     private static final String special = "";
